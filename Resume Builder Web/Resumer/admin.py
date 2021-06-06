dd419dc670f81ed639e1f9784bc2e24454c4aa2b
Gaurav Bhardwaj <61881543+sample1234-rgb@users.noreply.github.com>
2021-06-06 17:23:29 +0530

    Create admin.py

---
+++ Resume Builder Web/Resumer/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+admin.site.register(Contact)
+admin.site.register(Addresse)
+admin.site.register(Information)
+admin.site.register(Project)
+admin.site.register(Organization)
+admin.site.register(Education)
+admin.site.register(Experience)
+admin.site.register(Achievement)
+admin.site.register(Skill)
